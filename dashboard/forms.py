eb78ad531b850f66e7d2a244e6d638f1ffe86c0e
notfacebook16-dotcom <notfacebook16@gmail.com>
2026-06-10 03:13:03 +0600

    Delete dashboard directory

--- dashboard/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UserFile
-
-class FileUploadForm(forms.Form):
-    file = forms.FileField(
-        widget=forms.FileInput(attrs={'class': 'form-control', 'multiple': True})
-    )
--- dashboard/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# dashboard/utils/__init__.py
-# This file makes the 'utils' directory a Python package
-
-from .file_handler import FileHandler
-
-__all__ = ['FileHandler']
